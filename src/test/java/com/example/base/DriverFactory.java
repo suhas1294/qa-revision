805ecc954586d882b8b5b5b0fbdf50b83c8eb102
suhas1294 <suhasr94@gmail.com>
2021-02-09 16:06:27 +0530

    moved from desiredCapabilities to options

--- src/test/java/com/example/base/DriverFactory.java
+++ src/test/java/com/example/base/DriverFactory.java
@@ -1,31 +1,26 @@
 package com.example.base;
 
 import com.example.utils.PropertyReader;
+import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Properties;
 
 public class DriverFactory {
-    
-    public DesiredCapabilities capabilities;
     public WebDriver driver;
     private final static String configFile = "config.properties";
+    public MutableCapabilities capabilities;
     
     public WebDriver getDriver(String browser) throws IOException {
         Properties config = new PropertyReader(configFile).readPropertyFile();
         switch (browser) {
             case "firefox":
-                capabilities = DesiredCapabilities.firefox();
-                capabilities.setCapability(FirefoxOptions.FIREFOX_OPTIONS, OptionsManager.getFirefoxOptions());
+                capabilities = OptionsManager.getFirefoxOptions();
                 break;
             default:
-                capabilities = DesiredCapabilities.chrome();
-                capabilities.setCapability(ChromeOptions.CAPABILITY, OptionsManager.getChromeOptions());
+                capabilities = OptionsManager.getChromeOptions();
         }
         String driverNodeUrl = config.getProperty("driverNodeUrl");
         driver = new RemoteWebDriver(new URL(driverNodeUrl), capabilities);
